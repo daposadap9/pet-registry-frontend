22f2e5e8cef91c9a2cdb85992870e2c4686fa775
daposadap9 <diegoposada1999@gmail.com>
2024-09-08 15:33:38 -0500

    new changes

--- pages/Login.vue
+++ pages/Login.vue
@@ -43,7 +43,7 @@ export default {
   methods: {
     async login() {
       try {
-        const response = await axios.post(`${process.env.VUE_APP_API_URL}/login`, {
+        const response = await axios.post(`${runtimeConfig.apiURL}/login`, {
           email: this.email,
           password: this.password,
         });
