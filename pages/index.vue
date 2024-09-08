6c91e86411291f6d525099af48294a90d573cb3f
daposadap9 <diegoposada1999@gmail.com>
2024-09-08 17:07:47 -0500

    NEW CHANGES

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@ export default {
   components: {
     Home
   },
-  middleware: 'auth',
+  middleware: 'auth', // Middleware maneja la redirección
   data() {
     return {
       isAuthenticated: false
@@ -20,10 +20,8 @@ export default {
   },
   async created() {
     if (process.client) {
+      // Verifica el token en el cliente y actualiza el estado
       this.isAuthenticated = !!localStorage.getItem('token');
-      if (!this.isAuthenticated) {
-        this.$router.push('/login');
-      }
     }
   }
 };
