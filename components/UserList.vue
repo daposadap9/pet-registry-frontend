4a281ba5a807b53677efe3c96e0c1b2904c697ad
daposadap9 <diegoposada1999@gmail.com>
2024-09-22 16:45:34 -0500

    new changes

--- components/UserList.vue
+++ components/UserList.vue
@@ -1,15 +1,16 @@
 <template class="bg-chat-background">
-    <div class="bg-gray-800 text-white w-64 min-h-screen p-4 shadow-lg transition-all duration-300 ease-in-out">
+    <div :class="['bg-gray-800 text-white min-h-screen p-4 shadow-lg transition-all duration-300 ease-in-out', { 'w-full': isMobile, 'w-80': !isMobile }]">
       <h2 class="text-2xl font-bold mb-4">Usuarios</h2>
       <nav>
         <ul>
-          <li v-for="user in users" :key="user.id" class="mb-2 bg-gray-800">
-            <a @click="$emit('select-user', user)" class="block px-4 py-2 rounded hover:bg-gray-700 flex items-center cursor-pointer user-item">
+          <li v-for="user in users" :key="user.id" class="mb-2 bg-gray-800 border-b-2 border-b-slate-400 flex justify-between items-center">
+            <a @click="$emit('select-user', user)" class="block px-4 py-2 rounded hover:bg-gray-700 flex items-center cursor-pointer user-item w-full">
               <img :src="user.profilePic" alt="Profile Picture" class="w-8 h-8 rounded-full mr-2">
-              <div>
+              <div class="flex-1">
                 <span>{{ user.alias }}</span>
                 <p class="text-sm text-gray-400">{{ lastMessages[user.id] }}</p>
               </div>
+              <span class="text-xs text-gray-400 ml-2">{{ formatDate(lastMessageDates[user.id]) }}</span>
             </a>
           </li>
         </ul>
@@ -20,7 +21,38 @@
   <script>
   export default {
     name: 'UserList',
-    props: ['users', 'lastMessages']
+    props: ['users', 'lastMessages', 'lastMessageDates'],
+    data() {
+      return {
+        isMobile: false
+      };
+    },
+    mounted() {
+      this.checkViewport();
+      window.addEventListener('resize', this.checkViewport);
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.checkViewport);
+    },
+    methods: {
+      checkViewport() {
+        this.isMobile = window.innerWidth < 1024;
+      },
+      formatDate(timestamp) {
+        const date = new Date(timestamp);
+        const today = new Date();
+        const oneDay = 24 * 60 * 60 * 1000;
+        const oneWeek = 7 * oneDay;
+  
+        if (date.toDateString() === today.toDateString()) {
+          return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (today - date < oneWeek) {
+          return date.toLocaleDateString('es-ES', { weekday: 'long' });
+        } else {
+          return date.toLocaleDateString();
+        }
+      }
+    }
   };
   </script>
   
--- pages/Mensajes.vue
+++ pages/Mensajes.vue
@@ -5,16 +5,18 @@
         <Header />
         <div class="mx-auto px-4 py-6 mt-16 p-1 h-full bg-gray-900">
           <div class="hidden lg:flex">
-            <UserList :users="users" :lastMessages="lastMessages" @select-user="selectUser" />
+            <UserList :users="users" :lastMessages="lastMessages" :lastMessageDates="lastMessageDates" @select-user="selectUser" />
             <Chat :messages="messages" :sendMessage="sendMessage" :newMessage="newMessage" />
           </div>
-          <div class="lg:hidden">
-            <div class="tabs">
-              <button @click="activeTab = 'users'" :class="{ 'active': activeTab === 'users' }">Usuarios</button>
-              <button @click="activeTab = 'messages'" :class="{ 'active': activeTab === 'messages' }">Mensajes</button>
+          <div class="lg:hidden bg-blue-700">
+            <div class="tabs w-5">
+              <button @click="activeTab = 'users'" :class="{ 'active': activeTab === 'users' }">
+                <img src="~/assets/back.png" alt="Regresar" class="tab-icon" />
+                Regresar
+              </button>
             </div>
             <div class="bg-chat-background" v-if="activeTab === 'users'">
-              <UserList :users="users" :lastMessages="lastMessages" @select-user="selectUser" />
+              <UserList :users="users" :lastMessages="lastMessages" :lastMessageDates="lastMessageDates" @select-user="selectUser" />
             </div>
             <div v-if="activeTab === 'messages'">
               <Chat :messages="messages" :sendMessage="sendMessage" :newMessage="newMessage" />
@@ -50,13 +52,18 @@
           content: ''
         },
         activeTab: 'users',
-        lastMessages: {
-          1: 'Muy bien, gracias.',
-          2: 'Muy bien, gracias.',
-          3: 'Muy bien, gracias.'
-        },
+        lastMessages: {},
+        lastMessageDates: {},
         selectedUserAlias: ''
       };
+    },
+    watch: {
+      messages: {
+        handler() {
+          this.updateLastMessages();
+        },
+        deep: true
+      }
     },
     methods: {
       sendMessage() {
@@ -70,6 +77,7 @@
           timestamp: new Date().toISOString().slice(0, 19).replace('T', ' ')
         };
         this.messages.push(newMessage);
+        this.updateLastMessage('Usuario1', newMessage);
         this.newMessage.content = '';
       },
       selectUser(user) {
@@ -95,6 +103,33 @@
         this.selectedUserAlias = user.alias;
         this.activeTab = 'messages';
       },
+      updateLastMessage(userId, message) {
+        this.$set(this.lastMessages, userId, message.content);
+        this.$set(this.lastMessageDates, userId, message.timestamp);
+      },
+      updateLastMessages() {
+        this.users.forEach(user => {
+          const userMessages = this.messages.filter(message => message.sender === user.alias || message.senderAlias === user.alias);
+          if (userMessages.length > 0) {
+            const lastMessage = userMessages[userMessages.length - 1];
+            this.updateLastMessage(user.id, lastMessage);
+          }
+        });
+      },
+      formatDate(timestamp) {
+        const date = new Date(timestamp);
+        const today = new Date();
+        const oneDay = 24 * 60 * 60 * 1000;
+        const oneWeek = 7 * oneDay;
+  
+        if (date.toDateString() === today.toDateString()) {
+          return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (today - date < oneWeek) {
+          return date.toLocaleDateString('es-ES', { weekday: 'long' });
+        } else {
+          return date.toLocaleDateString();
+        }
+      },
       logout() {
         localStorage.removeItem('token');
         this.$router.push('/login'); 
@@ -113,15 +148,21 @@
   }
   
   .tabs {
-    @apply flex justify-around bg-blue-800 text-white p-4;
+    @apply flex justify-around text-white p-4 ;
   }
   
   .tabs button {
-    @apply flex-1 text-center p-2 rounded hover:bg-gray-700;
+    @apply flex-1 text-center p-2 rounded bg-blue-500 hover:bg-gray-500;
   }
   
-  .tabs button.active {
-    @apply bg-gray-700;
+ .tabs button.active {
+    @apply bg-blue-700;
+  } 
+  
+  .tab-icon {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
   }
   </style>
   
